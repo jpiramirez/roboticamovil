803661622cd5694bd12743a76e5270bba892a87a
Juan-Pablo Ramirez-Paredes <jpi.ramirez@ugto.mx>
2020-09-22 11:24:06 -0500

    Mod pioneersetpt for angle diff

--- pioneersetpt.py
+++ pioneersetpt.py
@@ -1,9 +1,28 @@
+"""
+Robot set-point control (regulation) using a Pioneer p3dx from CoppeliaSim
+
+Author: Juan-Pablo Ramirez-Paredes <jpi.ramirez@ugto.mx>
+Mobile Robotics course, University of Guanajuato (2020)
+"""
+
 import numpy as np
 import time
 import math as m
 import vrep # access all the VREP elements
+
+def angdiff(t1, t2):
+    """
+    Compute the angle difference, t2-t1, restricting the result to the [-pi,pi] range
+    """
+    # The angle magnitude comes from the dot product of two vectors
+    angmag = m.acos(m.cos(t1)*m.cos(t2)+m.sin(t1)*m.sin(t2))
+    # The direction of rotation comes from the sign of the cross product of two vectors
+    angdir = m.cos(t1)*m.sin(t2)-m.sin(t1)*m.cos(t2)
+    return m.copysign(angmag, angdir)
+
+
 vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) # start a connection
+clientID=vrep.simxStart('127.0.0.1',-1,True,True,5000,5) # start a connection
 if clientID!=-1:
 	print ('Connected to remote API server')
 else:
@@ -28,24 +47,26 @@ for i in range(16):
 ret, carpos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_streaming)
 ret, carrot = vrep.simxGetObjectOrientation(clientID, robot, -1, vrep.simx_opmode_streaming)
 
-Kv = 0.5;
-Kh = 2.5;
-xd = 0;
-yd = 0;
-hd = 0;
-r = 0.1;
-L = 0.2;
-errp = 10;
+# Controller gains (linear and heading)
+Kv = 0.5
+Kh = 2.5
+
+# xd and yd are the coordinates of the desired setpoint
+xd = -2
+yd = 0
+
+hd = 0
+r = 0.5*0.195
+L = 0.311
+errp = 10
 
 while errp > 0.1:
     ret, carpos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_blocking)
     ret, carrot = vrep.simxGetObjectOrientation(clientID, robot, -1, vrep.simx_opmode_blocking)
-    print(carpos)
-    print(carrot)
     errp = m.sqrt((xd-carpos[0])**2 + (yd-carpos[1])**2)
-    print(errp)
     angd = m.atan2(yd-carpos[1], xd-carpos[0])
-    errh = angd-carrot[2]
+    errh = angdiff(carrot[2], angd)
+    print('Distance to goal: {}   Heading error: {}'.format(errp, errh))
 
 # Uncomment for switched control
    # if errh > 2.0*m.pi/180.0:
@@ -63,7 +84,11 @@ while errp > 0.1:
     ur = v/r + L*omega/(2*r)
     errf = vrep.simxSetJointTargetVelocity(clientID, motorL, ul, vrep.simx_opmode_streaming)
     errf = vrep.simxSetJointTargetVelocity(clientID, motorR, ur, vrep.simx_opmode_streaming)
-    time.sleep(0.1)
+    #time.sleep(0.1)
 
-
+for i in range(100):
+    errf = vrep.simxSetJointTargetVelocity(clientID, motorL, 0, vrep.simx_opmode_streaming)
+    errf = vrep.simxSetJointTargetVelocity(clientID, motorR, 0, vrep.simx_opmode_streaming)
+    #time.sleep(0.1)
+    
 vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
