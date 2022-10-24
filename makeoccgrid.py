4bcf087e5f3035701a8b30a77b154ac5278a3c94
Juan-Pablo Ramirez-Paredes <jpi.ramirez@ugto.mx>
2022-10-24 13:36:09 -0500

    Fixed makeoccgrid.py using the Legacy RemoteAPI, updated for CoppeliaSim 4+

--- makeoccgrid.py
+++ makeoccgrid.py
@@ -4,7 +4,7 @@
 Occupancy grid creation using a Pioneer pd3x with ultrasonic sensors.
 
 Author: Juan-Pablo Ramirez-Paredes <jpi.ramirez@ugto.mx>
-Mobile Robotics course, University of Guanajuato (2020)
+Mobile Robotics course, University of Guanajuato (2022)
 """
 
 
@@ -12,9 +12,10 @@ import numpy as np
 import time
 import math as m
 import random
+import sys
 import matplotlib.pyplot as plt
 import os
-import sim as vrep # access all the VREP elements
+import sim  # access all the sim elements
 from skimage.draw import line
 
 def q2R(x,y,z,w):
@@ -31,8 +32,8 @@ def q2R(x,y,z,w):
     return R
 
 
-vrep.simxFinish(-1) # just in case, close all opened connections
-clientID=vrep.simxStart('127.0.0.1',-1,True,True,5000,5) # start a connection
+sim.simxFinish(-1) # just in case, close all opened connections
+clientID=sim.simxStart('127.0.0.1',-1,True,True,5000,5) # start a connection
 if clientID!=-1:
 	print ('Connected to remote API server')
 else:
@@ -40,22 +41,22 @@ else:
 	sys.exit("No connection")
 
 # Getting handles for the motors and robot
-err, motorL = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_blocking)
-err, motorR = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_blocking)
-err, robot = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx', vrep.simx_opmode_blocking)
+err, motorL = sim.simxGetObjectHandle(clientID, '/PioneerP3DX/leftMotor', sim.simx_opmode_blocking)
+err, motorR = sim.simxGetObjectHandle(clientID, '/PioneerP3DX/rightMotor', sim.simx_opmode_blocking)
+err, robot = sim.simxGetObjectHandle(clientID, '/PioneerP3DX', sim.simx_opmode_blocking)
 
 # Assigning handles to the ultrasonic sensors
 usensor = []
-for i in range(1,17):
-    err, s = vrep.simxGetObjectHandle(clientID, 'Pioneer_p3dx_ultrasonicSensor'+str(i), vrep.simx_opmode_blocking)
+for i in range(0,16):
+    err, s = sim.simxGetObjectHandle(clientID, '/PioneerP3DX/ultrasonicSensor['+str(i)+']', sim.simx_opmode_blocking)
     usensor.append(s)
 
 # Sensor initialization
 for i in range(16):
-    err, state, point, detectedObj, detectedSurfNormVec = vrep.simxReadProximitySensor(clientID, usensor[i], vrep.simx_opmode_streaming)
+    err, state, point, detectedObj, detectedSurfNormVec = sim.simxReadProximitySensor(clientID, usensor[i], sim.simx_opmode_streaming)
 
-ret, carpos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_streaming)
-ret, carrot = vrep.simxGetObjectOrientation(clientID, robot, -1, vrep.simx_opmode_streaming)
+ret, carpos = sim.simxGetObjectPosition(clientID, robot, -1, sim.simx_opmode_streaming)
+ret, carrot = sim.simxGetObjectOrientation(clientID, robot, -1, sim.simx_opmode_streaming)
 
 Kv = 0.5
 Kh = 2.5
@@ -79,8 +80,8 @@ t = time.time()
 
 initt = t
 niter = 0
-while time.time()-t < 300:
-    ret, carpos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_blocking)
+while time.time()-t < 30:
+    ret, carpos = sim.simxGetObjectPosition(clientID, robot, -1, sim.simx_opmode_blocking)
 
     xw = carpos[0]
     yw = carpos[1]
@@ -92,19 +93,19 @@ while time.time()-t < 300:
         yr = 100
     occgrid[yr-1, xr-1] = 0
 
-    ret, carrot = vrep.simxGetObjectOrientation(clientID, robot, -1, vrep.simx_opmode_blocking)
+    ret, carrot = sim.simxGetObjectOrientation(clientID, robot, -1, sim.simx_opmode_blocking)
 
     uread = []
     ustate = []
     upt = []
-    for i in range(0,16):
-       err, state, point, detectedObj, detectedSurfNormVec = vrep.simxReadProximitySensor(clientID, usensor[i], vrep.simx_opmode_buffer)
-       ret, objpos = vrep.simxGetObjectPosition(clientID, detectedObj, -1, vrep.simx_opmode_blocking)
+    for i in range(0,16,4):
+       err, state, point, detectedObj, detectedSurfNormVec = sim.simxReadProximitySensor(clientID, usensor[i], sim.simx_opmode_buffer)
+       ret, objpos = sim.simxGetObjectPosition(clientID, detectedObj, -1, sim.simx_opmode_blocking)
        uread.append(np.linalg.norm(point))
        upt.append(point)
        ustate.append(state)
-       ret, srot = vrep.simxGetObjectQuaternion(clientID, usensor[i], -1, vrep.simx_opmode_blocking)
-       ret, spos = vrep.simxGetObjectPosition(clientID, usensor[i], -1, vrep.simx_opmode_blocking)
+       ret, srot = sim.simxGetObjectQuaternion(clientID, usensor[i], -1, sim.simx_opmode_blocking)
+       ret, spos = sim.simxGetObjectPosition(clientID, usensor[i], -1, sim.simx_opmode_blocking)
        R = q2R(srot[0], srot[1], srot[2], srot[3])
        spos = np.array(spos).reshape((3,1))
        if i % 2 != 0:
@@ -142,40 +143,18 @@ while time.time()-t < 300:
            rows, cols = line(yr-1, xr-1, yo-1, xo-1)
            occgrid[rows, cols] = 0
 
-    # v = 0.5
-    # if v > 0.5:
-    #     v = 0.5
-
-    # omega = 0
-    # if omega > 2.5:
-    #     omega = 2.5
-    # elif omega < -2.5:
-    #     omega = -2.5
-
-    # if ustate[2] == True and uread[2] < 0.5:
-    #     print('Imminent collision at '+str(uread[4]))
-    #     omega = -1.5-0.1*random.random()
-    #     v = 0.01+0.01*random.random()
-    # if ustate[5] == True and uread[5] < 0.5:
-    #     print('Imminent collision at '+str(uread[4]))
-    #     omega = 1.5+0.1*random.random()
-    #     v = 0.01+0.01*random.random()
-
-    # ul = v/r - L*omega/(2*r)
-    # ur = v/r + L*omega/(2*r)
-
-    ul = 2.0
-    ur = 2.0
-    lgains = np.linspace(0,-1,8)
-    rgains = np.linspace(-1,0,8)
-    for i in range(8):
-        if ustate[i]:
-            ul = ul + lgains[i]*(1.0 - uread[i])
-            ur = ur + rgains[i]*(1.0 - uread[i])
+    ul = 1
+    ur = 1
+    lgains = np.linspace(0,-1,len(upt)//2)
+    rgains = np.linspace(-1,0,len(upt)//2)
+    for k in range(len(upt)//2):
+        if ustate[k]:
+            ul = ul + lgains[k]*(1.0 - uread[k])
+            ur = ur + rgains[k]*(1.0 - uread[k])
     print('lvel {}   rvel {}'.format(ul, ur))
 
-    errf = vrep.simxSetJointTargetVelocity(clientID, motorL, ul, vrep.simx_opmode_streaming)
-    errf = vrep.simxSetJointTargetVelocity(clientID, motorR, ur, vrep.simx_opmode_streaming)
+    errf = sim.simxSetJointTargetVelocity(clientID, motorL, ul, sim.simx_opmode_oneshot)
+    errf = sim.simxSetJointTargetVelocity(clientID, motorR, ur, sim.simx_opmode_oneshot)
 
     niter = niter + 1
 
@@ -187,6 +166,7 @@ print('Avg time per iteration ', (finalt-initt)/niter)
 plt.imshow(tocc+occgrid)
 plt.show()
 np.savetxt('map.txt', tocc+occgrid)
-errf = vrep.simxSetJointTargetVelocity(clientID, motorL, 0.0, vrep.simx_opmode_streaming)
-errf = vrep.simxSetJointTargetVelocity(clientID, motorR, 0.0, vrep.simx_opmode_streaming)
-vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot)
+for i in range(10):
+    errf = sim.simxSetJointTargetVelocity(clientID, motorL, 0.0, sim.simx_opmode_oneshot)
+    errf = sim.simxSetJointTargetVelocity(clientID, motorR, 0.0, sim.simx_opmode_oneshot)
+sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot)
